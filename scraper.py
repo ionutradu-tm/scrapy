02a51efda1cbbdc3f664a51e33ea4b9dea46deb7
iradu <ionut.radu@tremend.com>
2019-03-07 13:24:32 +0200

    - initial commit

--- scraper.py
+++ scraper.py
@@ -1,14 +1,13 @@
 import scrapy
 import sys
+import os
 from scrapy.crawler import CrawlerProcess
 from w3lib.http import basic_auth_header
 
 
-if len(sys.argv) == 1:
-    sys.exit("X-CACHE-UPDATER is missing")
 
-x_cache_updater_val = sys.argv[1]
-x_depth = sys.argv[2]
+x_cache_updater_val = os.environ["X-CACHE-UPDATER"]
+x_depth = os.environ["DEPTH"]
 
 print('X-CACHE-UPDATER value is ' + x_cache_updater_val)
 print('DEPTH_LIMIT value is ' + x_depth)
